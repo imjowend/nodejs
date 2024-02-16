0e0022ad625138c3363ec3498797d2c58aad063b
jowend <joaquinvcestudio@gmail.com>
2024-02-16 13:34:22 -0500

    process working directory

---
+++ midudev-curso-nodejs/7-process.js
@@ -0,0 +1,13 @@
+// argumentos de entrada
+console.log(process.argv);
+
+//controlar el proceso y su salida
+//process.exit(1)
+
+// podemos controlar los eventos del proceso
+// process.on('exit', () =>{
+//     // limpiar el proceso
+// })
+
+// current working directory
+console.log(process.cwd());
