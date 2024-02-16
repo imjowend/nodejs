9206d8be143ed8097086b1c637f8cb26c1acaf8a
jowend <joaquinvcestudio@gmail.com>
2024-02-15 20:43:25 -0500

    path y ls

--- midudev-curso-nodejs/6-path.js
+++ midudev-curso-nodejs/6-path.js
@@ -2,3 +2,15 @@ const path = require('node:path');
 
 // unir rutas con path.join
 console.log(path.sep);
+
+const filePath = path.join('path','package')
+
+console.log(filePath)
+
+const base = path.basename('/tmp/secret-file/password.txt')
+
+console.log(base)
+
+const extension = path.extname('image.jpg')
+
+console.log(extension)
