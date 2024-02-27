a188a99a5519dcfe5fe7881ef2b70a35284f1ae7
jowend <joaquinvcestudio@gmail.com>
2024-02-27 07:54:02 -0500

    bd

---
+++ microservicios-platzi-auth-redis/store/mysql.js
@@ -0,0 +1,10 @@
+const mysql = require('mysql');
+
+const config = require('../config')
+
+const dbconf = {
+    host: config.mysql.host,
+    user: config.mysql.user,
+    password: config.mysql.password,
+    database: config.api.mysql.database,
+}
