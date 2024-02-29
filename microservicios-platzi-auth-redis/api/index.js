89e12b2b65ec55ae3347777ff0d6dcb3b082deb3
jowend <joaquinvcestudio@gmail.com>
2024-02-28 20:26:32 -0500

    cambios

--- microservicios-platzi-auth-redis/api/index.js
+++ microservicios-platzi-auth-redis/api/index.js
@@ -8,6 +8,7 @@ const ingredient = require('./components/food/network.js');
 
 app.use('/api/ingredient', ingredient)
 
+// aca sabemos en que puerto escucha el programa
 app.listen(config.api.port, () =>{
     console.log('Api escuchando en el puerto: ', config.api.port);
 })
